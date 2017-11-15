873fe511c357020f3be963e0a64497ba0aec0a7c
Micah Halter <micah@mehalter.com>
2017-11-15 10:51:27 +0800

    Created simple sync script for dotfiles

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+repo="/home/micah/git/dotfiles/"
+files="./files"
+
+while read file
+do
+    filepath=`dirname $file|cut -d'/' -f2-`
+    filename=`basename "$file"`
+    if eval "test -e $file"; then
+        if [[ $filepath != "~" ]]; then
+            filepath=$filepath/
+            mkdir -p $repo$filepath
+        else
+            filepath=""
+        fi
+        if eval "test -f $file"; then
+            eval "rsync $file $repo$filepath$filename"
+        elif eval "test -d $file"; then
+            eval "rsync -r $file/ $repo$filepath$filename"
+        fi
+    fi
+done < $files
