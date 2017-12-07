e19675a912a2d661ad05b523d75d2e7b7bd64ca4
Micah Halter <micah@mehalter.com>
2017-12-08 01:58:35 +0800

    stupid notification idea.... fixed it now... much better

--- .offlineimapnotify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo `expr $(ls ~/.Mail/Gatech/INBOX/new|wc -l) + $(ls ~/.Mail/Gmail/INBOX/new|wc -l)` > ~/.Mail/newmail
-for i in {1..11}
-do
-  sleep 5
-  echo `expr $(ls ~/.Mail/Gatech/INBOX/new|wc -l) + $(ls ~/.Mail/Gmail/INBOX/new|wc -l)` > ~/.Mail/newmail
-done
