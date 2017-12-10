5d9417739d480e39cc53a00779a94a6134efc9b2
Micah Halter <micah@mehalter.com>
2017-12-10 14:21:32 +0800

    Added previous directory shortcut

--- .config/Scripts/shortcuts.py
+++ .config/Scripts/shortcuts.py
@@ -14,7 +14,7 @@ with open("/home/micah/.config/Scripts/folders") as fold:
         rang+=("map m"+line[0]+" shell mv %s "+line[1]+"\n")
         rang+=("map Y"+line[0]+" shell cp -r %s "+line[1]+"\n")
         #Adds the zsh shortcuts:
-        zsh+=("alias "+line[0]+"=\"cd "+line[1]+" && ls -a\"\n")
+        zsh+=("alias "+line[0]+"=\"cd "+line[1]+"; echo \$PWD\"\n")
 
 #Goes thru the config file file and adds the shortcuts to both zsh and ranger.
 
