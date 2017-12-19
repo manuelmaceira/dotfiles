7690c1b087c436c8eb971949c5c0b4da9deb9571
Micah Halter <micah@mehalter.com>
2017-12-19 13:35:58 +0800

    Added wal themed i3lock

--- .config/i3/scripts/lock/lock.sh
+++ .config/i3/scripts/lock/lock.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+. /home/micah/.cache/wal/colors.sh
+convert /home/micah/.config/i3/scripts/lock/lock.png -fuzz 50% -fill "$color4" -opaque "#30AF91" /tmp/out.png
+convert /tmp/out.png -fuzz 10% -fill "$color0" -opaque "#FFFFFF" /tmp/out.png
+convert /tmp/out.png -resize 256x256 /tmp/out.png
+
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+convert /tmp/screen.png /tmp/out.png -gravity center -composite -matte /tmp/screen.png
 #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 i3lock -u -i /tmp/screen.png
 rm /tmp/screen.png
