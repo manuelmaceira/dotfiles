286cde6ce1e9501c3b8db337148dd44fbb9dc3fd
Micah Halter <micah@mehalter.com>
2017-12-19 15:59:20 +0800

    Moved to i3lock-color and added colored indicator

--- .config/i3/scripts/lock/lock.sh
+++ .config/i3/scripts/lock/lock.sh
@@ -8,5 +8,17 @@ convert /tmp/lock.png -resize 256x256 /tmp/lock.png
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 convert /tmp/screen.png /tmp/lock.png -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
+
+i3lock -i /tmp/screen.png \
+  --radius=125 --ring-width=6 \
+  --insidecolor=00000000 --line-uses-inside \
+  --insidevercolor=00000000 --veriftext="" \
+  --insidewrongcolor=00000000 --wrongtext="" \
+  --ringcolor=${color4:1:6}ff \
+  --separatorcolor=${color4:1:6}ff \
+  --keyhlcolor=${color0:1:6}ff \
+  --bshlcolor=${color0:1:6}ff \
+  --ringvercolor=${color0:1:6}ff \
+  --ringwrongcolor=${color2:1:6}ff \
+
 rm /tmp/screen.png /tmp/lock.png
