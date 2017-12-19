8937879de2249369909447a4772e30d94d00fcd8
Micah Halter <micah@mehalter.com>
2017-12-19 13:36:57 +0800

    Removed uneeded line

--- .config/i3/scripts/lock/lock.sh
+++ .config/i3/scripts/lock/lock.sh
@@ -8,6 +8,5 @@ convert /tmp/out.png -resize 256x256 /tmp/out.png
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 convert /tmp/screen.png /tmp/out.png -gravity center -composite -matte /tmp/screen.png
-#dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 i3lock -u -i /tmp/screen.png
 rm /tmp/screen.png
