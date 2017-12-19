a43c86689b68fffd6c2d18a2a667e4594ad51681
Micah Halter <micah@mehalter.com>
2017-12-19 13:38:11 +0800

    Cleaned up

--- .config/i3/scripts/lock/lock.sh
+++ .config/i3/scripts/lock/lock.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 . /home/micah/.cache/wal/colors.sh
-convert /home/micah/.config/i3/scripts/lock/lock.png -fuzz 50% -fill "$color4" -opaque "#30AF91" /tmp/out.png
-convert /tmp/out.png -fuzz 10% -fill "$color0" -opaque "#FFFFFF" /tmp/out.png
-convert /tmp/out.png -resize 256x256 /tmp/out.png
+convert /home/micah/.config/i3/scripts/lock/lock.png -fuzz 50% -fill "$color4" -opaque "#30AF91" /tmp/lock.png
+convert /tmp/lock.png -fuzz 10% -fill "$color0" -opaque "#FFFFFF" /tmp/lock.png
+convert /tmp/lock.png -resize 256x256 /tmp/lock.png
 
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-convert /tmp/screen.png /tmp/out.png -gravity center -composite -matte /tmp/screen.png
+convert /tmp/screen.png /tmp/lock.png -gravity center -composite -matte /tmp/screen.png
 i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
+rm /tmp/screen.png /tmp/lock.png
