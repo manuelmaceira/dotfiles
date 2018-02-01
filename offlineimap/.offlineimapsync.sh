a125d1d0150f0cbdfd8418dd2041876655b1926a
Micah Halter <micah@mehalter.com>
2018-01-31 19:35:43 -0500

    worked on offlineimap sync script

--- offlineimap/.offlineimapsync.sh
+++ offlineimap/.offlineimapsync.sh
@@ -4,7 +4,7 @@ imapserver=$(cat ~/.offlineimaprc | grep remotehost | awk '{print $3}')
 imapactive=$(ps -ef | grep '/usr/bin/[o]fflineimap' | wc -l)
 netactive=$(ping -c3 $imapserver > /dev/null 2>&1 && echo up || echo down)
 
-mailsync="/usr/bin/offlineimap -u quiet -q"
+mailsync="/usr/bin/offlineimap"
 
 case $imapactive in
 '1')
