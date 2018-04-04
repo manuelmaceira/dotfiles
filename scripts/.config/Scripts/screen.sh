94cd8e4e2bbc335cc2a2309003f26c1d2da4a46b
Micah Halter <micah@mehalter.com>
2018-04-04 19:05:57 -0400

    Created more robuse screen switching script that will allow for on the fly screen configurations through command line arguments

--- scripts/.config/Scripts/screen.sh
+++ scripts/.config/Scripts/screen.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 
-left='HDMI-2'
-right='HDMI-3'
-laptop='LVDS-1'
+while getopts "l:r:h" opt; do
+  case ${opt} in
+    l )
+      left="$OPTARG"
+      ;;
+    r )
+      right="$OPTARG"
+      ;;
+    h )
+      echo "Usage:"
+      exit 0
+      ;;
+    \? )
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
 
-dual() {
-  if [[ ($(xrandr -q | grep $left\ con)) && ($(xrandr -q | grep $right\ con)) ]]; then
-    xrandr --output $left --auto --output $right --auto --right-of $left --output $laptop --off
+middle="$1"
+
+if [[ ("$middle" != "") && ($(xrandr -q |grep $middle\ con)) ]]; then
+  xrandr --output $middle --auto
+else
+  echo "Falling back to only laptop display" 1>&2
+  xrandr --output LVDS-1 --auto
+  xrandr --output $left --off
+  xrandr --output $right --off
+
+  ~/.config/polybar/launch.sh
+  feh --bg-scale ~/.config/wall
+
+  exit 0
+fi
+
+if [[ ("$left" != "") && ($(xrandr -q |grep $left\ con)) ]]; then
+  xrandr --output $left --auto --left-of $middle
+fi
+
+if [[ ("$right" != "") && ($(xrandr -q |grep $right\ con)) ]]; then
+  xrandr --output $right --auto --right-of $middle
+fi
+
+xrandr -q|grep '\bconnected\b'|cut -d' ' -f1|while read line; do
+  if [[ ("$line" != "$middle") && ("$line" != "$left") && ("$line" != "$right") ]]; then
+    xrandr --output $line --off
   fi
-}
-
-left() {
-  if [[ $(xrandr -q | grep $left\ con) ]]; then
-    xrandr --output $left --auto --output $laptop --off  --output $right --off
-  fi
-}
-
-right() {
-  if [[ $(xrandr -q | grep $right\ con) ]]; then
-    xrandr --output $right --auto --output $laptop --off  --output $left --off
-  fi
-}
-
-laptop() {
-  xrandr --output $laptop --auto --output $left --off  --output $right --off
-}
-
-case $1 in
-  d) dual ;;
-  l) left ;;
-  r) right ;;
-  s) laptop ;;
-  *) echo "Invalid parameter"
-esac
+done
 
 ~/.config/polybar/launch.sh
 feh --bg-scale ~/.config/wall
