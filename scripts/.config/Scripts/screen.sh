f3f9945e6760ed46165996d7d814fd3a5b1952d5
Micah Halter <micah@mehalter.com>
2018-04-04 19:18:53 -0400

    Fixed some edge cases and added usage guide

--- scripts/.config/Scripts/screen.sh
+++ scripts/.config/Scripts/screen.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+showusage() {
+  echo "Usage:"
+  echo "  screen.sh [options] main-display"
+  echo ""
+  echo "Options:"
+  echo "  -l <display>  Display to be used to the left of main-display"
+  echo "  -r <display>  Display to be used to the right of main-display"
+  echo "  -h            Show this screen."
+}
+
+disablerest() {
+  xrandr -q|grep '\bconnected\b'|cut -d' ' -f1|while read line; do
+    if [[ ("$line" != "$middle") && ("$line" != "$left") && ("$line" != "$right") ]]; then
+      xrandr --output $line --off
+    fi
+  done
+}
+
 while getopts "l:r:h" opt; do
   case ${opt} in
     l )
@@ -9,11 +27,12 @@ while getopts "l:r:h" opt; do
       right="$OPTARG"
       ;;
     h )
-      echo "Usage:"
+      showusage
       exit 0
       ;;
     \? )
       echo "Invalid Option: -$OPTARG" 1>&2
+      showusage
       exit 1
       ;;
   esac
@@ -24,31 +43,26 @@ middle="$1"
 
 if [[ ("$middle" != "") && ($(xrandr -q |grep $middle\ con)) ]]; then
   xrandr --output $middle --auto
+
+  if [[ ("$left" != "") && ($(xrandr -q |grep $left\ con)) ]]; then
+    xrandr --output $left --auto --left-of $middle
+  fi
+
+  if [[ ("$right" != "") && ($(xrandr -q |grep $right\ con)) ]]; then
+    xrandr --output $right --auto --right-of $middle
+  fi
+
+  disablerest
 else
   echo "Falling back to only laptop display" 1>&2
-  xrandr --output LVDS-1 --auto
+
+  disablerest
+
   xrandr --output $left --off
   xrandr --output $right --off
 
-  ~/.config/polybar/launch.sh
-  feh --bg-scale ~/.config/wall
-
-  exit 0
+  xrandr --output LVDS-1 --auto
 fi
-
-if [[ ("$left" != "") && ($(xrandr -q |grep $left\ con)) ]]; then
-  xrandr --output $left --auto --left-of $middle
-fi
-
-if [[ ("$right" != "") && ($(xrandr -q |grep $right\ con)) ]]; then
-  xrandr --output $right --auto --right-of $middle
-fi
-
-xrandr -q|grep '\bconnected\b'|cut -d' ' -f1|while read line; do
-  if [[ ("$line" != "$middle") && ("$line" != "$left") && ("$line" != "$right") ]]; then
-    xrandr --output $line --off
-  fi
-done
 
 ~/.config/polybar/launch.sh
 feh --bg-scale ~/.config/wall
