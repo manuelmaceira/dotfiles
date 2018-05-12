32e43ccba9a20fd5866a18d7d67709cc55e347de
Micah Halter <micah@mehalter.com>
2018-05-12 10:11:46 -0400

    Cleaned up shortcuts script and configs/folders lists

--- scripts/.config/Scripts/shortcuts.sh
+++ scripts/.config/Scripts/shortcuts.sh
@@ -14,31 +14,31 @@ echo "# ranger shortcuts" > $ranger_shortcuts
 
 writeDirs() {
   echo "alias $1='cd $2 && ls'" >> $zsh_shortcuts
-	echo "map g$1 cd $2" >> $ranger_shortcuts
-	echo "map t$1 tab_new $2" >> $ranger_shortcuts
-	echo "map m$1 shell mv %s $2" >> $ranger_shortcuts
-	echo "map Y$1 shell cp -r %s $2" >> $ranger_shortcuts
+  echo "map g$1 cd $2" >> $ranger_shortcuts
+  echo "map t$1 tab_new $2" >> $ranger_shortcuts
+  echo "map m$1 shell mv %s $2" >> $ranger_shortcuts
+  echo "map Y$1 shell cp -r %s $2" >> $ranger_shortcuts
 }
 
 writeConfs() {
-	echo "alias $1='nvim $2'" >> $zsh_shortcuts
-	echo "map $1 shell nvim $2" >> $ranger_shortcuts
+  echo "alias $1='nvim $2'" >> $zsh_shortcuts
+  echo "map $1 shell nvim $2" >> $ranger_shortcuts
 }
 
 IFS=$'\n'
 set -f
 for line in $(cat "$folders"); do
-	line=$(echo $line | sed 's/#.*//')
-	key=$(echo $line | awk '{print $1}')
-	dir=$(echo $line | awk '{print $2}')
-	[ "$dir" == "" ] || writeDirs $key $dir
+  line=$(echo $line | sed 's/#.*//')
+  key=$(echo $line | awk '{print $1}')
+  dir=$(echo $line | awk '{print $2}')
+  [ "$dir" == "" ] || writeDirs $key $dir
 done
 
 set -f
 for line in $(cat "$configs");
 do
-	line=$(echo $line | sed 's/#.*//')
-	short=$(echo $line | awk '{print $1}')
-	conf=$(echo $line | awk '{print $2}')
-	[ "$conf" == "" ] || writeConfs $short $conf
+  line=$(echo $line | sed 's/#.*//')
+  short=$(echo $line | awk '{print $1}')
+  conf=$(echo $line | awk '{print $2}')
+  [ "$conf" == "" ] || writeConfs $short $conf
 done
